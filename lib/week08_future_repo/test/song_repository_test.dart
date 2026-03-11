48fd43588abc0ba43123f5a0c539c0e8b19d6de8
Sotheara Kon <sothearakon73@gmail.com>
2026-03-11 09:45:35 +0700

    week08_future_repo

---
+++ lib/week08_future_repo/test/song_repository_test.dart
@@ -0,0 +1,29 @@
+import 'package:challenges/week08_future_repo/data/repositories/songs/song_repository_mock.dart';
+import 'package:challenges/week08_future_repo/model/songs/song.dart';
+
+void main() async {
+  //   Instantiate the  song_repository_mock
+  SongRepositoryMock songRepositoryMock = SongRepositoryMock();
+
+  // Test both the success and the failure of the post request
+
+  // Handle the Future using 2 ways  (2 tests)
+  // - Using then() with .catchError().
+  // - Using async/await with try/catch.
+
+  // songRepositoryMock
+  //     .fetchSongById("s22")
+  //     .then((value) {
+  //       print("fetched song ${value?.title}");
+  //     })
+  //     .catchError((error) {
+  //       print("this song id is not found, $error");
+  //     });
+
+  try {
+    Song? value = await songRepositoryMock.fetchSongById("s11");
+    print("fetched song ${value?.title}");
+  } catch (error) {
+    print("this song id is not found, $error");
+  }
+}
